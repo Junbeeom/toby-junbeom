4a64df99a2ac2e11b5a9d698cfb1399d6d0bcade
jojunbeom <wns_p@naver.com>
2025-07-04 23:04:50 +0900

    init

---
+++ src/main/java/tobyspring/studytobyddd/StudyTobyDddApplication.java
@@ -0,0 +1,13 @@
+package tobyspring.studytobyddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTobyDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyTobyDddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tobyspring/studytobyddd/StudyTobyDddApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.studytobyddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTobyDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
